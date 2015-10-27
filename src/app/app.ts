714dd08578b02fa947aaf7c9f20d4f772456299e
Rafael.Glanzner <rafael.audy@gmail.com>
2015-10-27 20:18:34 +0000

    Adding two way data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
 
 class Hero {
   id: number;
@@ -7,7 +7,16 @@ class Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ng-model)]="hero.name" placeholder="name">
+        </div>
+        `,
+    directives: [FORM_DIRECTIVES]
 })
 class AppComponent {
     public title = 'Tour of Heroes';
@@ -16,7 +25,5 @@ class AppComponent {
         name: 'Windstorm'
     };
 }
-
-
 
 bootstrap(AppComponent);
