1b48e3c311813208dda27ba5e9f055b5d7ac7bea
Rafael.Glanzner <rafael.audy@gmail.com>
2015-10-27 20:06:58 +0000

    Adding a class for the hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,22 @@
 import {Component, bootstrap} from 'angular2/angular2';
 
+class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App was just refreshed</h1>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
-class AppComponent { }
+class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+
 
 bootstrap(AppComponent);
